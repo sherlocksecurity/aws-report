0d380acbc08c61bacedea4ae508f63294db22544
gmdutra <gmdutra.eu@gmail.com>
2019-12-15 02:07:13 -0300

    Add volumes analyzer

--- aws_report.py
+++ aws_report.py
@@ -4,15 +4,17 @@ from modules.s3 import S3Analyzer
 from modules.iam import IamAnalyzer
 from modules.securitygroups import SgAnalyzer
 from modules.elasticip import ElasticIpAnalyzer
+from modules.volumes import VolumesAnalyzer
 from modules.banner import Banner
 
 @click.command()
 @click.option("--s3", is_flag=True, help="Search buckets public in s3")
-@click.option("--iam", is_flag=True, help="analyze iam users based on creation date")
+@click.option("--iam", is_flag=True, help="Search iam users based on creation date")
 @click.option('--sg', is_flag=True, help="Search security groups with inbound rule 0.0.0.0")
 @click.option('--elasticip', is_flag=True, help="Search elastic IP not associated")
+@click.option('--volumes', is_flag=True, help="Search volumes available")
 
-def main(s3, iam, sg, elasticip):
+def main(s3, iam, sg, elasticip, volumes):
 
     if s3:
         s3 = S3Analyzer()
@@ -29,6 +31,10 @@ def main(s3, iam, sg, elasticip):
     elif elasticip:
         ip = ElasticIpAnalyzer()
         ip.find_elastic_dissociated()
+
+    elif volumes:
+        vol = VolumesAnalyzer()
+        vol.find_volumes_available()
 
 if __name__=='__main__':
     print(Banner.banner)
---
+++ modules/volumes.py
@@ -0,0 +1,14 @@
+import boto3
+
+from modules.colors import bcolors
+
+class VolumesAnalyzer():
+    def __init__(self):
+        self.ec2 = boto3.client('ec2')
+
+    def find_volumes_available(self):
+        volumes = self.ec2.describe_volumes()
+        for volume in volumes['Volumes']:
+            volume_id = volume['VolumeId']
+            if len(volume['Attachments']) == 0:
+                print("{0}[INFO]{1} EBS Volume {2} available".format(bcolors.WARNING, bcolors.ENDC, volume_id))
