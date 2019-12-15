d8e732786aa3f6dc701ea2edd4ecfaa7eedcc09a
gmdutra <gmdutra.eu@gmail.com>
2019-12-15 13:26:02 -0300

    Refactoring: remove function len()

--- modules/volumes.py
+++ modules/volumes.py
@@ -10,5 +10,5 @@ class VolumesAnalyzer():
         volumes = self.ec2.describe_volumes()
         for volume in volumes['Volumes']:
             volume_id = volume['VolumeId']
-            if len(volume['Attachments']) == 0:
+            if not volume['Attachments']:
                 print("{0}[INFO]{1} EBS Volume {2} available".format(bcolors.WARNING, bcolors.ENDC, volume_id))
